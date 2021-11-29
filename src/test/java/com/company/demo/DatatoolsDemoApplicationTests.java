32b765d5d7e28dbe221ac44ed307e1e6fa7ab8a6
Nadezhda Shatalova <n.shatalova@haulmont.com>
2021-11-29 17:24:39 +0400

    first commit

---
+++ src/test/java/com/company/demo/DatatoolsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatatoolsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
